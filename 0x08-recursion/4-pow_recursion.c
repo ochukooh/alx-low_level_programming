d3bd7c27f3abcd55f4dcf87adcb7e4d85c0e9b0c
ochukooh <74387488+ochukooh@users.noreply.github.com>
2023-06-10 19:24:32 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
  * _pow_recursion - function
- * x: variable
- * y: variable
+ * @x: variable
+ * @y: variable
  * Return: void
  */
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
 	{
-		return 1.0;
+		return (1.0);
 	}
 	else if (y > 0)
 	{
-		return x * _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 	else
 	{
-		return 1.0 / (x * _pow_recursion(x, -y - 1));
+		return (1.0 / (x * _pow_recursion(x, -y - 1)));
 	}
 }

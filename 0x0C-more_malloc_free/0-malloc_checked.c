b218bd18dd9102a36d0db67d174509bf28112a2b
ochuko <chunou95@gmail.com>
2023-06-16 19:11:18 -0700

    s12

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include<stdlib.h>
+void *malloc_checked(unsigned int b) {
+  void *ptr = malloc(b);
+  if (ptr == NULL) {
+    // Handle error
+    exit(98);
+  }
+  return ptr;
+}
+

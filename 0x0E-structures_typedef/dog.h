6de92fbfc694d41ce472dfda61f528c222dad45f
ochuko <chunou95@gmail.com>
2023-06-20 18:49:29 -0700

    tas0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - d9g class
+ * @name: member
+ * @age: member
+ * @owner: member
+ */
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+#endif
